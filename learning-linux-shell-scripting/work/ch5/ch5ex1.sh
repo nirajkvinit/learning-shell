27616e5c9e8f1b4ac55639616016c80723b11ad5
Niraj Kumar <nirajkvinit@gmail.com>
2017-07-13 00:49:43 +0530

    Ch5 completed

---
+++ learning-linux-shell-scripting/work/ch5/ch5ex1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script clears the window, greets the user,
+# and displays the current date and time.
+clear
+echo "SCRIPT BEGINS"
+echo "Hello $LOGNAME"
+echo
+
+echo "Today's date and time:"
+date  # Display current date and time
+echo  # Will print empty line
+
+my_num=50
+my_day="Sunday"
+
+echo "The value of my_num is $my_num"
+echo "The value of my_day is $my_day"
+echo
+
+echo "Script Finished!!"
+echo 
